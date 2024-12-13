2a022788ef91120ed89a1b85cf936aafe68e9a0c
harisrahim5011 <harisrahim5011@gmail.com>
2024-12-13 11:59:19 -0500

    general user book retrival implemented

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,19 +7,22 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  res.send(books[isbn]);
+//   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
