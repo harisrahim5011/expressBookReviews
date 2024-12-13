6a6371617240f6bef6c53856f3905c385ce1136e
harisrahim5011 <harisrahim5011@gmail.com>
2024-12-13 12:24:37 -0500

    task-4 book by title is implemented

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -49,7 +49,20 @@ public_users.get('/author/:author', function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
     //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const title = req.params.title;
+    const booksByTitle = [];
+    // Iterate over the keys of the 'books' object
+    for (let key in books) {
+        if (books[key].title === title) {
+            booksByTitle.push(books[key]);
+        }
+    }
+
+    if (booksByTitle.length > 0) {
+        return res.status(200).json(booksByTitle);
+    } else {
+        return res.status(404).json({ message: "No books found by this title" });
+    }
 });
 
 //  Get book review
